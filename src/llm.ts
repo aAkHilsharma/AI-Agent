ddf339025e5c817fc78483debc50731fc7d5c604
aAkHilsharma <akhilsharma6721@gmail.com>
2024-11-27 00:29:15 +0530

    Add memory and chat capability

--- src/llm.ts
+++ src/llm.ts
@@ -1,17 +1,13 @@
+import type { AIMessage } from '../types';
 import { openai } from './ai';
 
 export const runLLM = async ({
-    userMessage
-}: { userMessage: string }) => {
+    messages
+}: { messages: AIMessage[] }) => {
     const response = await openai.chat.completions.create({
         model: 'gpt-4o-mini',
         temperature: 0.1,
-        messages: [
-            {
-                role: 'user',
-                content: userMessage
-            }
-        ]
+        messages,
     })
 
     return response.choices[0].message.content;
---
+++ src/memory.ts
@@ -0,0 +1,42 @@
+import { JSONFilePreset } from 'lowdb/node'
+import type { AIMessage } from '../types'
+import { v4 as uuid } from 'uuid'
+
+export type MessageWithData = AIMessage & {
+    id: string;
+    createdAt: string
+}
+
+export const addMetadata = (message: AIMessage): MessageWithData => ({
+    ...message,
+    id: uuid(),
+    createdAt: new Date().toISOString()
+})
+
+export const removeMetaData = (message: MessageWithData): AIMessage => {
+    const { id, createdAt, ...messageWithoutMetaData } = message
+    return messageWithoutMetaData;
+}
+
+type Data = {
+    messages: MessageWithData[]
+}
+
+const defaultData: Data = { messages: [] }
+
+export const getDb = async() => {
+    const db = await JSONFilePreset<Data>('db.json', defaultData);
+    return db;
+}
+
+export const addMessages = async (messages: AIMessage[]) => {
+    const db = await getDb();
+
+    db.data.messages.push(...messages.map(addMetadata))
+    await db.write();
+}
+
+export const getMessages = async() => {
+    const db = await getDb();
+    return db.data.messages.map(removeMetaData)
+}
